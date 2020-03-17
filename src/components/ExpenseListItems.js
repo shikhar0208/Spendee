7e84915af7f69326621b57eeb5779d476deeb5e8
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2020-03-18 00:31:22 +0530

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItems.js
+++ src/components/ExpenseListItems.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-  
-const ExpenseListItems = ({ id, description, amount, createdAt}) => (
-    <div>
-        <Link to={`/edit/${id}`}>
-            <h3>{description}</h3>
-        </Link>
-        <p>{amount} - {createdAt} </p>
-    </div>
+import React from "react";
+import { Link } from "react-router-dom";
+import moment from 'moment';
+import numeral from 'numeral';
+import numeralSetup from '../numeral/customNumeral';
+
+numeralSetup();
+
+const ExpenseListItems = ({ id, description, amount, createdAt }) => (
+  <div>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>
+      {numeral(amount).format('$0,0.00')}
+       -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
+  </div>
 );
 
 export default ExpenseListItems;
--- src/tests/components/ExpenseListItems.test.js
+++ src/tests/components/ExpenseListItems.test.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import ExpenseListItems from '../../components/ExpenseListItems';
-import expenses from '../fixtures/expenses';
+import React from "react";
+import { shallow } from "enzyme";
+import ExpenseListItems from "../../components/ExpenseListItems";
+import expenses from "../fixtures/expenses";
 
-
-test('should render ExpenseListItem correctly', () => {
-    const wrapper = shallow(<ExpenseListItems {...expenses[1]} />);
-    expect(wrapper).toMatchSnapshot();
-})
+test("should render ExpenseListItem correctly", () => {
+  const wrapper = shallow(<ExpenseListItems {...expenses[1]} />);
+  expect(wrapper).toMatchSnapshot();
+});
+    
